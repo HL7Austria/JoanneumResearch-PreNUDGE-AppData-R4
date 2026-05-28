8b1bb3c9b28f7d8ea080ad9c678a1bd6fa663da5
Thomas Truskaller <thomas.truskaller@joanneum.at>
2026-05-28 11:57:29 +0200

    #5 Mapping for sleep quality

---
+++ validations/run-mappings.bat
@@ -0,0 +1,6 @@
+ malac-hd -m ..\input\maps\at-prenudge-sleep-quality-base.map -m ..\input\maps\at-prenudge-sleep-quality-mapping-from-whoqol-bref.map -ti ..\fsh-generated\resources\QuestionnaireResponse-whoqol-bref-response-example.json  -to 01_sleep_quality_whoqol_bref_obs.json
+
+ malac-hd -m ..\input\maps\at-prenudge-sleep-quality-base.map -m ..\input\maps\at-prenudge-sleep-quality-mapping.map -ti ..\fsh-generated\resources\QuestionnaireResponse-sleep-quality-response-dissatisfied-example.json  -to 02_leep_quality_dissatisfied_obs.json
+
+ malac-hd -m ..\input\maps\at-prenudge-sleep-quality-base.map -m ..\input\maps\at-prenudge-sleep-quality-mapping.map -ti ..\fsh-generated\resources\QuestionnaireResponse-sleep-quality-response-satisfied-example.json  -to 03_sleep_quality_satisfied_obs.json
+
